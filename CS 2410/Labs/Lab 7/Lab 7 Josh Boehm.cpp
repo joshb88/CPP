52645a7b3d2ae9b9fa0d481ed0f0808dc67ac556
Joshua Boehm <bhm0814@gmail.com>
2022-10-06 12:20:15 -0500

    Changes to be committed: 	modified:   CS 2410/Labs/Lab 7/Lab 7 Josh Boehm.cpp

--- CS 2410/Labs/Lab 7/Lab 7 Josh Boehm.cpp
+++ CS 2410/Labs/Lab 7/Lab 7 Josh Boehm.cpp
@@ -1,8 +1,35 @@
 /*
 Josh Boehm
-4 October 2022
+6 October 2022
 CS 2410
 Lab 7
+
+(The Account class) Design a class name Account that contains :
+
+After LAB 6 is done , we like to use the Account class created above to simulate an ATM machine.  
+Create 10 accounts in an array with id 0-9 and initial balance $100.  
+The system prompts the user to enter an id , if the id is entered incorrectly, ask the user to enter a correct one.  
+Once an id is accepted , the main menu is displayed (look at sample below) .
+ 
+You can enter a choice
+1 for viewing the current Balance
+2 for withdrawing money
+3 for depositing money
+4 for exiting the main menu.
+ 
+Once you exit the system will prompt for an id again .  
+So once the system starts it will not stop.
+ 
+Sample menu
+ 
+Enter an id :   4      ENTER
+Main Menu
+1. Check Balance
+2. Withdraw
+3. Deposit
+4. Exit
+Enter a Choice   ENTER
+The Balance is 100.00
 */
 
 #include <iostream>
@@ -14,9 +41,15 @@ Lab 7
 
 using namespace std;
 
+const int NUM_OF_ACCOUNTS = 10;
+
 int main()
 {
-    Account test;
-    test.setId(100);
-    cout << test.getId();
+    Account bank_accounts[NUM_OF_ACCOUNTS];
+    for (int i = 0; i > NUM_OF_ACCOUNTS; i++)
+    {
+        bank_accounts[i].setId(i);
+        bank_accounts[i].setBalance(100);
+        bank_accounts[i].setAnnualInterestRate(.025);
+    }
 }
