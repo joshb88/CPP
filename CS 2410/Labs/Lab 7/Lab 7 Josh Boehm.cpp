03af1f2757053cb01028f93dd0012c3e0639b7e1
Joshua Boehm <bhm0814@gmail.com>
2022-10-13 18:21:36 -0500

    added account.h in comments

--- CS 2410/Labs/Lab 7/Lab 7 Josh Boehm.cpp
+++ CS 2410/Labs/Lab 7/Lab 7 Josh Boehm.cpp
@@ -89,7 +89,6 @@ int idInputVerified()
         cin.clear();
         cin.ignore(256,'\n');
         cin >> idcheck;
-
     }
 
     return idcheck;
@@ -189,3 +188,39 @@ void mainMenu(Account a[])
         mainMenu(a);
     }
 }
+
+/*
+#ifndef ACCOUNT
+#define ACCOUNT
+
+class Account
+{
+    private:
+        int id; 
+        double balance;
+        double annualInterestRate;
+
+    public:
+        Account()
+        {
+            id = 0;
+            balance = 0;
+            annualInterestRate = 0;
+        }
+        Account(int setid, double setbal, double setrate)
+            :id(setid), balance(setbal), annualInterestRate(setrate)
+        {}
+        int getId()                                 const{           return id;          }
+        double getBalance()                         const{       return balance;         }
+        double getAnnualInteresteRate()             const{  return annualInterestRate;   }
+        double getMonthlyInterestRate()             const{ return annualInterestRate/12; }
+        
+        void setId(int setid)                            {         id = setid;           }
+        void setBalance(double setbalance)               {      balance = setbalance;    }
+        void setAnnualInterestRate(double setAIR)        {  annualInterestRate = setAIR; }
+        void withdrawl(double amount)                    {      balance -= amount;       }
+        void deposit(double amount)                      {      balance += amount;       }
+};
+
+#endif // ACCOUNT
+*/
