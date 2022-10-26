9604fcd6a4520428b21117ae9ad0fea67123c2b9
Joshua Boehm <bhm0814@gmail.com>
2022-10-26 13:58:14 -0500

    new homeworks

---
+++ CS 2410/Homework/Homework 5/Homework 5 Josh Boehm.cpp
@@ -0,0 +1,27 @@
+/*
+Josh Boehm
+26 October 2022
+CS 2410
+Program 5
+
+For this program we are using objects, class, Inheritance, Polymorphism, and virtual function.   
+(Programming Challenge 1 & #3 Page 982-Chapter 15  E9)
+Design a class named Employee.  The class should keep the following information:
+
+* Employee name
+* Employee number
+* Hire date
+
+Write one or more constructors, and the appropriate  accessors and mutator functions, for the class.  
+Next write a class named   ProductionWorker that is derived from   Employee class. 
+The   ProductionWorker  class should have member variables to hold the following information:
+
+* Shift (an integer)
+* Hourly pay rate (a double)
+
+The workday is divided into two shifts: day and night.  
+The shift variable hold an integer value representing the shift that the employee works.  
+The day shift is shift 1, and the night shift is shift 2.  
+Write one or more constructors, and the appropriates accessor  and mutator functions, 
+for the class by witting a program that uses a   ProductionWorker object  
+*/
---
+++ CS 2410/Homework/Homework 6/Homework 6 Josh Boehm.cpp
@@ -0,0 +1,11 @@
+/*
+Joshua Boehm
+26 October 2022
+CS 2410
+Program 6
+
+Use Program 5 and 
+Now in a particular factory , a team leader is an hourly paid production worker who leas a small team.  In addition to hourly pay, team leaders earn a fixed  monthly  bonus.  Team leaders are required to attend a minimum number of hours of training per year.  
+Design a   TeamLeader class that extends   ProductionWorker class  you designed in part 1 of this assignment.   The    TeamLeader  class should have member variable for the monthly bonus amount, the required number of training hours, and the number of training hours that the team leader has attended.
+Write one or more constructors and the appropriate accessor and mutator functions for the class.  Demonstrate the class by writing a program that uses a   TeamLeader object.
+*/
