bfd2d15291e3cffefbeae53d391de187beae61bb
Joshua Boehm <bhm0814@gmail.com>
2022-11-10 17:39:05 -0600

    new section for weekly questions

---
+++ CS 2410/Sample Questions/Question 1/question 1.cpp
@@ -0,0 +1,18 @@
+/*
+Write a template function to return maximum parameter type of two (regardless of type). 
+Write a test program to validate your code
+*/
+
+#include <iostream>
+#include <string>
+
+template <typename T> T maximum(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+
+int main()
+{
+    std::cout << maximum('j','g') << std::endl;
+    std::cout << maximum(15,6) << std::endl;
+}
