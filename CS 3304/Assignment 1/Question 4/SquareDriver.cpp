edb86d4ab3c7f3a0628cafd92ad9b075779fac13
Joshua Boehm <bhm0814@gmail.com>
2023-02-13 19:46:41 -0600

    typo

--- CS 3304/Assignment 1/Question 4/SquareDriver.cpp
+++ CS 3304/Assignment 1/Question 4/SquareDriver.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include "Square.h"
 #include <string>
+
 using namespace std;
 
 int main()
@@ -18,7 +19,7 @@ int main()
     d.display();
     cout << a.getSideLength() << endl;
     cout << b.getBottomLeftX() << endl;
-    cout << c.getBottomLeftY() << endl;s
+    cout << c.getBottomLeftY() << endl;
     c.setSideLength(-7);
     d.setSideLength(10);
     d.setBottomLeftX(-10);
