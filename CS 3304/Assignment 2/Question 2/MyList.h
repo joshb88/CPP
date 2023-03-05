981978cbf528a0f8991e060e81921b6d45ff63e1
Josh B <89416369+joshb88@users.noreply.github.com>
2023-03-05 16:39:43 -0600

    mylist.h

---
+++ CS 3304/Assignment 2/Question 2/MyList.h
@@ -0,0 +1,135 @@
+#ifndef MYLIST
+#define MYLIST
+#include <iostream>
+
+//MyCon class
+template<class T>
+class MyList
+{
+private:
+    T *my_array;
+    int my_size;
+    int my_capacity;
+public:
+    MyList(int capacity);
+    int getSize() const;
+    int getCapacity() const;
+    void add(T element);
+    T getMin();
+    T at(int index);
+    void removeMin();
+    void display();
+    T sum();
+    bool search(T element);
+    void clear();
+};
+
+template<class T>MyList<T>::MyList(int capacity)
+{
+    if (capacity < 0) 
+    { 
+        my_capacity = 5;
+    }
+
+    else
+    {
+        my_capacity = capacity;
+    }
+    my_array = new T[my_capacity]{0};
+    my_size = 0;
+}
+
+template<class T> int MyList<T>::getSize() const
+{ 
+    return my_size;
+}
+
+template<class T> int MyList<T>::getCapacity() const
+{
+     return my_capacity;
+}
+
+template<class T>void MyList<T>::add(T element)
+{
+    if (my_size == my_capacity)
+    {
+        my_capacity = my_capacity * 2;
+        T *temp = new T[my_capacity]{0};
+        for (int i = 0; i < my_size; i++)
+        {
+            temp[i] = my_array[i];
+        }
+        delete[] my_array;
+        my_array = temp;
+    }
+   if (element > my_array[0])
+        {
+            my_array[my_size] = element;
+            my_size++;
+        }
+    if (element < my_array[0])
+    {
+        T placeholder;
+        placeholder = my_array[0];
+        my_array[0] = element;
+        my_size++;
+        my_array[my_size-1] = placeholder;
+        
+    }
+}
+
+template<class T> T MyList<T>::getMin()
+{
+    try
+    {
+        if (my_size == 0) 
+            throw("");
+        else
+        {
+            return my_array[0];
+        }
+    }
+    catch(...)
+    {
+        std::cout << "Error: No values in the list. Returning -1." << std::endl;
+        return -1;
+    } 
+}
+
+template<class T> T MyList<T>::at(int index)
+{
+    try
+    {
+        if (index > my_size)
+            throw("");
+        else
+        {
+            return my_array[index];
+        }
+    }
+    catch(...)
+    {
+        std::cout << "Error: Specified index does not exist in the list. Returning -1." << std::endl;
+        return -1;
+    }
+    
+}
+
+template<class T> void removeMin()
+{
+    //this is a test
+}
+
+//function to display element of the container
+template<class T> void MyList<T>::display()
+{
+    std::cout << "[";
+    for(int i=0; i < my_size; i++)
+    {
+        std::cout << my_array[i];
+        if (i != my_size-1) {std::cout << ", "; }
+    }
+    std::cout << "]" << std::endl;
+}
+
+#endif // MYLIST
