841858a2e15236d159742701149228239054a82f
Josh B <89416369+joshb88@users.noreply.github.com>
2023-04-26 22:06:53 -0500

    test

--- CS 3304/Assignments 3 and 4/Question 3/test.cpp
+++ CS 3304/Assignments 3 and 4/Question 3/test.cpp
@@ -1,34 +1,18 @@
 #include <iostream>
+#include <iomanip>
+#include <algorithm>
+
 using namespace std;
 
-// Hash function H(x) = 3*x + 2 (mod 13)
-int hashFunc(int x) {
-    return (3*x + 2) % 13;
-}
-
-// Insert an integer in the hash table
-void insert(int arr[], int x) {
-    int idx = hashFunc(x);  // Calculate the hash value
-    while (arr[idx] != 0) {  // Handle collisions using linear probing
-        idx = (idx + 1) % 13;
-    }
-    arr[idx] = x;  // Insert the integer
-}
-
-int main() {
-    int arr[13] = {0};  // Create an array of 13 elements, initialized to 0
-    int values[] = {26, 42, 5, 44, 92, 38, 40, 45, 12, 57, 80, 31};
-    int n = sizeof(values) / sizeof(values[0]);  // Calculate the size of the array
-
-    // Insert the given integers in the order given
-    for (int i = 0; i < n; i++) {
-        insert(arr, values[i]);
-    }
-
-    // Display the final hash table
-    for (int i = 0; i < 13; i++) {
-        cout << i << ": " << arr[i] << endl;
-    }
-
-    return 0;
+int main()
+{
+    /*
+    Q1 (15 pts). Quick sort. Given the array of numbers, show the elements of the array after each
+    split operation of quick sort. The algorithm will sort the array in increasing order. Select the first
+    element as pivot in each split. For instance, use 49 as pivot in the first iteration and show the
+    array elements after split. Then, in the second iteration, apply split operation to left sublist and
+    right sublist. You can continue splits until you have 0 or 1 element in sublists.
+    i 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    x[i] 39 82 27 21 46 54 61 25 13 29 92 24 66 55 44 33
+    */
 }
